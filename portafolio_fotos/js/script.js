69be827b9d566c63a4cfa2454160c925cb644410
ghaalam <49927084+ghaalam@users.noreply.github.com>
2024-12-24 14:01:52 -0500

    Add files via upload

---
+++ portafolio_fotos/js/script.js
@@ -0,0 +1,10 @@
+// Script para el menú hamburguesa
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('open');
+        hamburger.classList.toggle('toggle');
+    });
+});
